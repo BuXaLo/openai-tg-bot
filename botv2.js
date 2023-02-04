0038c13a21f878d9d3e2a164792fa69bce73da07
BuXaLo <buxalo666@hotmail.com>
2023-02-05 01:55:12 +0300

    Create botv2.js
    add catch errors function

---
+++ botv2.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const BOT_USERNAME = process.env.BOT_USERNAME;
+
+const bot = new TelegramBot(BOT_TOKEN, {polling: true});
+
+bot.onText(new RegExp(`@${BOT_USERNAME} chat`), async (msg) => {
+  const chatId = msg.chat.id;
+  const prompt = msg.text.split('chat')[1].trim();
+
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/completions',
+      {
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 3900
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  
+    const generatedText = response.data.choices[0].text;
+  
+    // Send the generated text back to the chat
+    bot.sendMessage(chatId, generatedText);
+  } catch (error) {
+    // Handle the error and send an error message to the chat
+    console.error(error);
+    bot.sendMessage(chatId, 'An error occurred while processing your request. Please try again later.');
+  }
+});
+bot.onText(new RegExp(`@${BOT_USERNAME} pic`), async (msg) => {
+  const chatId = msg.chat.id;
+  const prompt = msg.text.split('pic')[1].trim();
+
+  try {
+    // Call OpenAI API to generate an image
+    const response = await axios.post(
+      'https://api.openai.com/v1/images/generations',
+      {
+        model: 'image-alpha-001',
+        prompt: prompt,
+        n: 1,
+        size: '1024x1024'
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  
+    const imageUrl = response.data.data[0].url;
+  
+    // Send the generated image back to the chat
+    bot.sendPhoto(chatId, imageUrl);
+  } catch (error) {
+    // Handle the error and send an error message to the chat
+    console.error(error);
+    bot.sendMessage(chatId, 'An error occurred while processing your request. Please try again later.');
+  }
+});
